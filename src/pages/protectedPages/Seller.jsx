0f5f03aeca3e446341c3b94ea7f5ef330b004413
KohKeira <kohkeira996@gmail.com>
2024-06-27 14:26:37 +0800

    Responsive desgin for protected pages

--- src/pages/protectedPages/Seller.jsx
+++ src/pages/protectedPages/Seller.jsx
@@ -10,10 +10,10 @@ const Seller = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100 ">
-      <div className="flex flex-col justify-center items-center text-center w-full max-w-sm bg-white rounded-lg py-8 px-12">
-        <h1 className="text-5xl font-bold mb-8  ">Seller</h1>
-        <h3 className="text-2xl  mb-8 ">Welcome {user.username}</h3>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 px-6 sm:px-8 lg:px-16">
+      <div className="flex flex-col justify-center items-center text-center w-full max-w-sm lg:max-w-md bg-white rounded-lg p-8 lg:p-12">
+        <h1 className="text-3xl font-bold mb-8 lg:text-5xl">Seller Page</h1>
+        <h3 className="text-xl mb-8 lg:text-3xl ">Welcome {user.username}</h3>
         <p className="text-gray-500 text-md text-center mb-8">
           This is the Seller Page. You can list your products here.
         </p>
