304f419d09750408877a548f9f0dc39d6f06179e
KohKeira <kohkeira996@gmail.com>
2024-07-01 17:14:03 +0800

    Change message of button depending if user is logged in.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import AppContext from "../AppContext";
 
 const Home = () => {
+  const { user } = useContext(AppContext);
   return (
     <div className="min-h-screen flex justify-center bg-gray-100">
       <div className="flex flex-col justify-center items-center max-w-lg mx-10">
@@ -10,12 +13,12 @@ const Home = () => {
         <h3 className="text-xl large:text-3xl font-medium text-center mb-8">
           You can sell, buy and earn delivery commission on this platform
         </h3>
-        <Link to="/login">
+        <Link to={user ? `/${user.role}` : "/login"}>
           <button
             aria-label="Login Button"
             className="w-full text-lg text-white bg-purple-400 hover:bg-indigo-700 py-2 px-4 rounded"
           >
-            Login to continue
+            {user ? `View your page` : "Login to continue"}
           </button>
         </Link>
       </div>
