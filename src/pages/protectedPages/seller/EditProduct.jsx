9e9cf000bc51145e845da1b7053f5287e38e86af
KohKeira <kohkeira996@gmail.com>
2024-07-25 11:33:59 +0800

    Edit and Create Page for seller

---
+++ src/pages/protectedPages/seller/EditProduct.jsx
@@ -0,0 +1,63 @@
+import { Form, Formik } from "formik";
+import TextField from "../../../components/formComponents/TextField";
+import * as Yup from "yup";
+import { TextArea } from "../../../components/formComponents/TextArea";
+
+export const EditProduct = ({ ...product }) => {
+  const editProduct = (values) => {};
+
+  return (
+    <div className=" min-h-screen  bg-gray-100 px-6 sm:px-12 lg:px-20 pt-24 pb-4">
+      <h2 className="text-3xl font-bold mb-6 text-center">Edit Product</h2>
+
+      <div className="flex flex-col justify-center items-center max-w-lg bg-white rounded-lg px-6 md:px-12 m-auto py-4">
+        <Formik
+          initialValues={{
+            name: product.name,
+            desc: product.desc,
+            price: product.price,
+            quantity: product.quantity,
+            image: product.image,
+          }}
+          validationSchema={Yup.object({
+            name: Yup.string().required("Please enter the product name"),
+            desc: Yup.string().required("Please enter the product description"),
+            price: Yup.number()
+              .test(
+                "is-decimal",
+                "The amount should be a decimal with maximum two digits after comma",
+                (value) => (value + "").match(/^\d*\.\d{2,}$/)
+              )
+              .required("Please enter the product price"),
+            quantity: Yup.number()
+              .positive()
+              .integer("Please enter a whole number")
+              .required("Please enter the product quantity"),
+            image: Yup.mixed().required("Please select an image"),
+          })}
+          onSubmit={editProduct}
+        >
+          <Form className="w-full flex flex-col justify-center">
+            <TextField name="name" label="Name" type="text"></TextField>
+            <TextArea name="desc" label="Description" type="text"></TextArea>
+            <TextField name="price" label="Price" type="number"></TextField>
+            <TextField
+              name="quantity"
+              label="Quantity"
+              type="number"
+            ></TextField>
+            <TextField name="image" label="Image" type="file"></TextField>
+
+            <button
+              aria-label="Login Button"
+              type="submit"
+              className="w-full bg-purple-500 hover:bg-indigo-700 text-white font-bold text-xl py-1 rounded my-8"
+            >
+              Edit
+            </button>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/protectedPages/seller/Seller.jsx
@@ -0,0 +1,87 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import AppContext from "../../../AppContext";
+
+const Seller = () => {
+  const { user, setUser } = useContext(AppContext);
+
+  return (
+    <div className=" min-h-screen  bg-gray-100 px-6 sm:px-12 lg:px-20 pt-24">
+      <div className="w-full">
+        <h1 className="text-xl lg:text-3xl font-bold bg-clip-text mb-6">
+          My Products
+        </h1>
+        <Link to={"/seller/product"}>
+          <button className="bg-blue-300 hover:bg-blue-500 p-2 rounded mb-4">
+            Add
+          </button>
+        </Link>
+
+        <table className="table-auto border text-left w-full mb-10">
+          <thead>
+            <tr>
+              <th className="border p-2">name</th>
+              <th className="border p-2">image</th>
+              <th className="border p-2">description</th>
+              <th className="border p-2">price</th>
+              <th className="border p-2">quantity</th>
+
+              <th className="border p-2">actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border p-2">name</td>
+              <td className="border p-2">
+                <img
+                  src={
+                    "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+                  }
+                  alt={"name"}
+                  height={100}
+                  width={100}
+                />
+              </td>
+              <td className="border p-2">description</td>
+              <td className="border p-2">${"price"}</td>
+              <td className="border p-2">quantity</td>
+
+              <td className="border p-2">
+                <Link to={"/seller/product/" + 1}>
+                  <button className="bg-yellow-400 hover:bg-yellow-500 p-2 rounded mr-2">
+                    Edit
+                  </button>
+                </Link>
+
+                <button className="bg-red-400 hover:bg-red-500 p-2 rounded">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <h1 className="text-xl lg:text-3xl font-bold bg-clip-text mb-6">
+          Orders
+        </h1>
+        <table className="table-auto border text-left w-full">
+          <thead>
+            <tr>
+              <th className="border p-2">user</th>
+              <th className="border p-2">product</th>
+              <th className="border p-2">status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border p-2">name</td>
+
+              <td className="border p-2">product</td>
+              <td className="border p-2">statusyou</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default Seller;
